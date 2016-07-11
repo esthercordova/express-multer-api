cbddeea9ea8f118972a8e092f8b8566a82225b19
esthercordova <estherpiacordova@gmail.com>
2016-07-11 09:38:11 -0400

    Add the upload information to mongoose

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const fs = require('fs');
+
+const mongoose = require('../app/middleware/mongoose');
+const Upload = require('../app/models/upload');
+
 const uploader = require('../lib/aws-s3-upload');
 
 let filename = process.argv[2] || '';
+let comment =  process.argv[3] || 'No comment';
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -20,5 +25,13 @@ const readFile = (filename) => {
 readFile(filename)
 .then(uploader.prepareFile)
 .then(uploader.awsUpload)
+.then((response) => {
+  let upload = {
+    location: response.Location,
+    comment: comment,
+  };
+  return Upload.create(upload);
+})
 .then(console.log)
-.catch(console.error);
+.catch(console.error)
+.then(() => mongoose.connection.close());
